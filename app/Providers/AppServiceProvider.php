5f22e1799128ffe5d18d336415d4f981d441d7cd
Erdyni <halhin3001@yandex.ru>
2026-01-25 21:56:56 +0800

    breeze установка

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace app\Providers;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,8 +18,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+
+        public function boot(): void
     {
-        //
+     //
     }
+
 }
